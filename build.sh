3f1568c83b5a3a6455907125a3d5f490ac6c7791
UtkarshNandyAviz <utkarsh.nandy@aviznetworks.com>
2024-07-08 10:03:51 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
+echo "PROBLEM HERE!"
 set -e # exit on error
 
 function usage {
